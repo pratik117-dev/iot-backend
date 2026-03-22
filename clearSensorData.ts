eb7c4cd8a7a1b5a167dfa72cc86c92b9e9c3073a
pratik117-dev <pratikpoudel1110@gmail.com>
2026-03-22 10:29:18 +0545

    modified

--- clearSensorData.ts
+++ clearSensorData.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.sensorData.deleteMany({});
-  console.log("✅ All sensor data cleared");
-}
+// async function main() {
+//   await prisma.sensorData.deleteMany({});
+//   console.log("✅ All sensor data cleared");
+// }
 
-main()
-  .catch((e) => {
-    console.error(e);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
